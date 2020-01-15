9984f20c5b6c06cea7b07702ce37c8ad5f8ead50
oiprado <oiprado@gmail.com>
2020-01-15 13:42:58 -0500

    Adding components structure packages

---
+++ backend/perfiltic/rest-api/src/main/java/com/perfiltic/oiprado/PerfilTICApplication.java
@@ -0,0 +1,20 @@
+package com.perfiltic.oiprado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableResourceServer
+@EnableScheduling
+@EnableTransactionManagement
+public class PerfilTICApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfilTICApplication.class, args);
+    }
+
+}
