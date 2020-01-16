2a39e00c0bafcf4ac07c5212416ad3087fd61e59
oiprado <oiprado@gmail.com>
2020-01-15 23:35:31 -0500

    Adding angular project

---
+++ fontend/ecommerce/src/app/shared/component/category/category.component.ts
@@ -0,0 +1,50 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+    
+    ngOnInit(): void {
+        
+    }
+    
+    categories = [
+        {
+            "id": 1,
+            "name": "Celulares",
+            "categories": [
+                {
+                    "id": 2,
+                    "name": "Accesorios",
+                    "categories": []
+                },
+                {
+                    "id": 3,
+                    "name": "Celulares y Teléfonos",
+                    "categories": [
+                        {
+                            "id": 5,
+                            "name": "Celular Huawei",
+                            "categories": []
+                        },
+                        {
+                            "id": 6,
+                            "name": "iPhone",
+                            "categories": []
+                        }
+                    ]
+                },
+                {
+                    "id": 4,
+                    "name": "Wearables",
+                    "categories": []
+                }
+            ]
+        }
+    ];
+
+}
---
+++ fontend/ecommerce/src/app/shared/component/component.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CategoryComponent } from './category/category.component';
+ 
+@NgModule({
+    declarations: [
+      CategoryComponent
+    ],
+    exports: [
+        CategoryComponent
+    ]
+    ,
+    imports: [
+      
+    ],
+    providers: []
+  })
+  export class ComponentModule { }
